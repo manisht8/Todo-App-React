aa6a006fe710ebb4a0a9d99948ec1d99192a605e
Manish Tulsiani <manishtulsiani.08@gmail.com>
2023-07-14 00:58:03 +0530

    save to LocalStorage, Mark as Complete feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import './App.css';
 import TaskInput from './TaskInput.js';
 import Task from './Task.js';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
+  // monitor changes on state - save to localStorage
+  useEffect(() => {
+    if (todos.length === 0) return;
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    const savedTodos = JSON.parse(localStorage.getItem('todos'));
+    if (savedTodos) {
+      setTodos(savedTodos);
+    }
+  }, []);
+
   function handleAddTask(taskName) {
-    if(taskName)
-      setTodos([...todos, {name: taskName, isCompleted: false}]);
+    if (taskName)
+      setTodos([...todos, { name: taskName, isCompleted: false }]);
+  }
+
+  function handleCompleteTask(taskIndex, status) {
+    const tempTodos = [...todos];
+    tempTodos[taskIndex].isCompleted = status;
+    setTodos(tempTodos);
   }
 
   return (
     <div className="App">
       <div className="tasks-container">
         <h2>To-Do List</h2>
-        <TaskInput onTaskAdd={(taskName) => handleAddTask(taskName) }/>
+        <TaskInput onTaskAdd={(taskName) => handleAddTask(taskName)} />
         {
-          todos.map((task) => (
-            <Task {...task}/>
+          todos.map((task, index) => (
+            <Task key={index} {...task} onStatusUpdate={(status) => handleCompleteTask(index, status)} />
           ))
         }
       </div>
--- src/Task.js
+++ src/Task.js
@@ -1,32 +1,31 @@
-import { useState } from "react";
-// import { isCompositeComponent } from "react-dom/test-utils";
+import { useState, useEffect } from "react";
 
-function Task({name, isCompleted}) {
-    const [checked, setChecked] = useState(false);
+function Task({ name, isCompleted, onStatusUpdate }) {
+  const [checked, setChecked] = useState(isCompleted);
 
-    function handleChecked(isTaskCompleted) {
-        if(isTaskCompleted)
-            setChecked(true);
-        else
-            setChecked(false);
-    }
-    
-    return (
-        <div className="task">
-            <div>
-                <input 
-                    type="checkbox" 
-                    className="checkbox"
-                    onChange={() => handleChecked({isCompleted})} 
-                    value={checked}/>
-                <span> {name} </span>
-            </div>
-            <div>
-                <button className="button">Edit</button>
-                <button className="button">Delete</button>
-            </div>
-        </div>
-    );
+  function handleChecked() {
+    const updatedChecked = !checked;
+    setChecked(updatedChecked);
+    onStatusUpdate(updatedChecked);
+  }
+
+  return (
+    <div className={'task ' + (isCompleted ? 'complete' : 'not-completed')}>
+      <div>
+        <input
+          type="checkbox"
+          className="checkbox"
+          checked={checked}
+          onChange={handleChecked}
+        />
+        <span>{name}</span>
+      </div>
+      <div>
+        <button className="button">Edit</button>
+        <button className="button">Delete</button>
+      </div>
+    </div>
+  );
 }
 
 export default Task;
